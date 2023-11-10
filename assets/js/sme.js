134939864f6013b894ebe3eeb0f3941bcd07d1ab
Kusal Amarasinghe <kusal4978@gmail.com>
2023-11-10 21:56:29 +0530

    Latest

--- assets/js/sme.js
+++ assets/js/sme.js
@@ -14,121 +14,111 @@ document.getElementById('mobileCloseBtn').onclick = function () {
 
 // Products //
 
-document.getElementById('dataOne').onclick = function () {
-    document.getElementById('dataOneSec').style.display = "inline";
-    document.getElementById('voBoxSec').style.display = "none";
-    document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
-    document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('cloudPos').style.display = "none";
-    document.getElementById('notUpdated').style.display = "none";
-}
-
 document.getElementById('voBox').onclick = function () {
-    document.getElementById('voBoxSec').style.display = "inline";
+    document.getElementById('voBoxSec').style.display = "inherit";
     document.getElementById('notUpdated').style.display = "none";
     document.getElementById('dataOneSec').style.display = "none";
     document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
     document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
     document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('cloudPos').style.display = "none";
-}
-
-document.getElementById('hostingCub').onclick = function () {
-    document.getElementById('hostingCubSec').style.display = "inline";
-    document.getElementById('voBoxSec').style.display = "none";
-    document.getElementById('notUpdated').style.display = "none";
-    document.getElementById('dataOneSec').style.display = "none";
-    document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
     document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
     document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('cloudPos').style.display = "none";
-}
-
-document.getElementById('msmE').onclick = function () {
-    document.getElementById('notUpdated').style.display = "inline";
-    document.getElementById('voBoxSec').style.display = "none";
-    document.getElementById('dataOneSec').style.display = "none";
-    document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
-    document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('cloudPos').style.display = "none";
-}
-
-document.getElementById('cloudPos').onclick = function () {
-    document.getElementById('notUpdated').style.display = "inline";
-    document.getElementById('voBoxSec').style.display = "none";
-    document.getElementById('dataOneSec').style.display = "none";
-    document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
-    document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('cloudPos').style.display = "none";
-}
-
-document.getElementById('peotvBiz').onclick = function () {
-    document.getElementById('notUpdated').style.display = "inline";
-    document.getElementById('voBoxSec').style.display = "none";
-    document.getElementById('dataOneSec').style.display = "none";
-    document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
-    document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('peotvBiz').style.display = "none";
 }
 
 document.getElementById('sltUnivo').onclick = function () {
     document.getElementById('voBoxSec').style.display = "none";
-    document.getElementById('dataOneSec').style.display = "none";
-    document.getElementById('sltUnivoSec').style.display = "inline";
-    document.getElementById('msmESec').style.display = "none";
-    document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('peotvBiz').style.display = "none";
     document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "inherit";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
 }
 
-document.getElementById('lteFlash').onclick = function () {
-    document.getElementById('lteFlashSec').style.display = "inline";
-    document.getElementById('notUpdated').style.display = "none";
+document.getElementById('cloudPos').onclick = function () {
     document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
     document.getElementById('dataOneSec').style.display = "none";
     document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
     document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
     document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "inherit";
     document.getElementById('bizTunesSec').style.display = "none";
-    document.getElementById('peotvBiz').style.display = "none";
+}
+
+document.getElementById('dataOne').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "inherit";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('hostingCub').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "inherit";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('peotvBiz').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "inherit";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
 }
 
 document.getElementById('bizTunes').onclick = function () {
-    document.getElementById('notUpdated').style.display = "inline";
     document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
     document.getElementById('dataOneSec').style.display = "none";
     document.getElementById('sltUnivoSec').style.display = "none";
-    document.getElementById('msmESec').style.display = "none";
     document.getElementById('hostingCubSec').style.display = "none";
-    document.getElementById('peotvBizSec').style.display = "none";
-    document.getElementById('bizTunesSec').style.display = "none";
     document.getElementById('lteFlashSec').style.display = "none";
-    document.getElementById('peotvBiz').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "inherit";
 }
 
+document.getElementById('lteFlash').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "inherit";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('other').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "inherit";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
 
