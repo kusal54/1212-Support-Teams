cc26bbe92f7b99d326eceecdfa9fb4bee3d0afef
Kusal Amarasinghegit config --global user.name Kusal <kusal4978@gmail.com>
2025-02-16 17:30:41 +0530

    BB

---
+++ assets/js/script-srm.js
@@ -0,0 +1,225 @@
+// Mobile Nav Start//
+<<<<<<< HEAD
+document.getElementById('hamburgerMenu').onclick = function () {
+=======
+document.getElementById('hamburgerMenu').onclick = function(){
+>>>>>>> cce7c8481268000118572bf9b65ebaf677110b27
+    document.getElementById('mobileCloseBtn').style.display = "inline";
+    document.getElementById('hamburgerMenu').style.display = "none";
+    document.getElementById('hamMenu').style.display = "flex";
+    document.getElementById('mobileNav').style.borderBottom = "#ffffff";
+    document.getElementById('hero-section').style.display = "none";
+    document.getElementById('channel-section').style.display = "none";
+    document.getElementById('footer-section').style.display = "none";
+    document.getElementById('teamSection').style.display = "none";
+    document.getElementById('targetsection').style.display = "none";
+    document.getElementById('answersection').style.display = "none";
+<<<<<<< HEAD
+
+}
+
+document.getElementById('mobileCloseBtn').onclick = function () {
+=======
+    
+}
+
+document.getElementById('mobileCloseBtn').onclick = function(){
+>>>>>>> cce7c8481268000118572bf9b65ebaf677110b27
+    document.getElementById('mobileCloseBtn').style.display = "none";
+    document.getElementById('hamburgerMenu').style.display = "inline";
+    document.getElementById('hamMenu').style.display = "none";
+    document.getElementById('hero-section').style.display = "inherit";
+    document.getElementById('channel-section').style.display = "inherit";
+    document.getElementById('footer-section').style.display = "inherit";
+    document.getElementById('teamSection').style.display = "inherit";
+    document.getElementById('targetsection').style.display = "inherit";
+    document.getElementById('answersection').style.display = "inherit";
+}
+
+
+
+// Mobile Nav End//
+
+
+
+// Sample Answers Start//
+document.getElementById('tabStart').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "inline";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabNc').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "inline";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabFaults').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "inline";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabBilling').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "inline";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabPackages').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "inline";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabPackageChange').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "inline";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabJob').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "inline";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabFeedbacks').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "inline";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabTeleshop').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "inline";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabSelfServices').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "inline";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabGb').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "inline";
+    document.getElementById('tblOther').style.display = "none";
+}
+
+document.getElementById('tabOther').onclick = function () {
+    document.getElementById('tblStartUp').style.display = "none";
+    document.getElementById('tblNc').style.display = "none";
+    document.getElementById('tblFaults').style.display = "none";
+    document.getElementById('tblBilling').style.display = "none";
+    document.getElementById('tblPackages').style.display = "none";
+    document.getElementById('tblPackageChange').style.display = "none";
+    document.getElementById('tblJob').style.display = "none";
+    document.getElementById('tblFeedbacks').style.display = "none";
+    document.getElementById('tblTeleshop').style.display = "none";
+    document.getElementById('tblSelfService').style.display = "none";
+    document.getElementById('tblGb').style.display = "none";
+    document.getElementById('tblOther').style.display = "inline";
+}
+// Sample Answers End//
---
+++ assets/js/sme.js
@@ -0,0 +1,124 @@
+// Mobile Nav Start//
+document.getElementById('hamburgerMenu').onclick = function () {
+    document.getElementById('mobileCloseBtn').style.display = "inline";
+    document.getElementById('hamburgerMenu').style.display = "none";
+    document.getElementById('hamMenu').style.display = "flex";
+    document.getElementById('mobileNav').style.borderBottom = "#ffffff";
+}
+
+document.getElementById('mobileCloseBtn').onclick = function () {
+    document.getElementById('mobileCloseBtn').style.display = "none";
+    document.getElementById('hamburgerMenu').style.display = "inline";
+    document.getElementById('hamMenu').style.display = "none";
+}
+
+// Products //
+
+document.getElementById('voBox').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "inherit";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('sltUnivo').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "inherit";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('cloudPos').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "inherit";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('dataOne').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "inherit";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('hostingCub').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "inherit";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('peotvBiz').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "inherit";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('bizTunes').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "inherit";
+}
+
+document.getElementById('lteFlash').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "none";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "inherit";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
+document.getElementById('other').onclick = function () {
+    document.getElementById('voBoxSec').style.display = "none";
+    document.getElementById('notUpdated').style.display = "inherit";
+    document.getElementById('dataOneSec').style.display = "none";
+    document.getElementById('sltUnivoSec').style.display = "none";
+    document.getElementById('hostingCubSec').style.display = "none";
+    document.getElementById('lteFlashSec').style.display = "none";
+    document.getElementById('peotvBizSec').style.display = "none";
+    document.getElementById('cloudPosSec').style.display = "none";
+    document.getElementById('bizTunesSec').style.display = "none";
+}
+
